11dc0a1e5da5f06d8c2ea170e37b89960c03cc43
yonasadiel <yonazadielwiguna@gmail.com>
2017-06-26 19:29:29 +0700

    this reference in about and help pm

--- src/personal.js
+++ src/personal.js
@@ -31,7 +31,7 @@ module.exports = {
   invalidCommand : function() {
     let reply_text = '';
     reply_text += 'Invalid Command';
-    return client.replyMessage(reply_text);
+    return this.client.replyMessage(reply_text);
   },
 
   aboutCommand : function() {
@@ -39,7 +39,7 @@ module.exports = {
     reply_text = 'Resistance Game!\n';
     // todo : make a help text
     //        link into github guide
-    return client.replyMessage(help_text);
+    return this.client.replyMessage(help_text);
   },
 
   helpCommand : function() {
